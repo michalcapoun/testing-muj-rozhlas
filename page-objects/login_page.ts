08c9ab39dca0832fa3159d7da8124e944bb1ec14
michalcapoun <michal.capoun@gmail.com>
2024-09-02 13:41:29 +0200

    add logout tests

--- page-objects/login_page.ts
+++ page-objects/login_page.ts
@@ -3,19 +3,27 @@ import { Locator, Page } from "@playwright/test";
 export class LoginPage {
   readonly page: Page;
   readonly url = "https://www.mujrozhlas.cz/user/login";
+  readonly registerLink: Locator;
   readonly usernameInput: Locator;
   readonly passwordInput: Locator;
   readonly loginButton: Locator;
+  readonly forgottenPasswordLink: Locator;
 
   constructor(page: Page) {
     this.page = page;
+    this.registerLink = page.locator(".u-font-bold");
     this.usernameInput = page.locator("#edit-name");
     this.passwordInput = page.locator("#edit-pass");
     this.loginButton = page.locator("//button[@type='submit']");
+    this.forgottenPasswordLink = page.locator(".f-login__link");
   }
 
   async openMujRozhlas() {
     await this.page.goto(this.url);
+  }
+
+  async clickRegister() {
+    await this.registerLink.click();
   }
 
   async fillUsername(username: string) {
@@ -28,6 +36,10 @@ export class LoginPage {
 
   async clickLogin() {
     await this.loginButton.click();
+  }
+
+  async clickForgottenPassword() {
+    await this.forgottenPasswordLink.click();
   }
 
   async login(username: string, password: string) {
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,37 @@
 import { test, expect } from "@playwright/test";
+import { LoginPage } from "../page-objects/login_page";
 
-console.log(process.env.EMAIL);
-console.log(process.env.PASSWORD);
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
 
-test("Should log in", async ({ page }) => {});
+if (!email || !password) {
+  throw new Error("Environment variables EMAIL and PASSWORD must be set.");
+}
+
+test("Login", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.openMujRozhlas();
+  await loginPage.fillUsername(email);
+  await loginPage.fillPassword(password);
+  await loginPage.clickLogin();
+});
+
+test("Succesful logout", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.login(email, password);
+  await page.locator('a[href="/user/logout"]').click();
+  await expect(page).toHaveURL("https://www.mujrozhlas.cz/user/logout/confirm");
+  await page.locator('//button[@type="submit"]').click();
+});
+
+test("Cancelled logout", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.login(email, password);
+  await page.locator('a[href="/user/logout"]').click();
+  await expect(page).toHaveURL("https://www.mujrozhlas.cz/user/logout/confirm");
+  await page.locator("#edit-cancel").click();
+  await expect(page).toHaveURL("https://www.mujrozhlas.cz/");
+});
